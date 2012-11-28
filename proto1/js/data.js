dfceb332a54d592d431130f1120ade5ad1189e55
sepans <sepans@sepans.com>
2012-11-28 14:59:59 -0500

    missing data file and jquery template js files

---
+++ proto1/js/data.js
@@ -0,0 +1,37 @@
+var videoInfo = {'id':'vid1', 'url': '/vid1.mp4', 'events': [ 
+						{'id':'e1', 'timestamp': '0', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video','name': 'video 3','url': '/vid3.mp4', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video','name': 'video 4','url': '/vid4.mp4', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '8', 'related_objects' : [
+							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.7' },
+							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video','name': 'video 5', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video','name': 'video 6', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
+							]
+						},
+						{'id':'e1', 'timestamp': '15', 'related_objects' : [
+							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video','name': 'video 4', 'url': '/vid4.mp4', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '22', 'related_objects' : [
+							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.7' },
+							{'id': 'vid3', 'type':'video','name': 'video 5', 'url': '/vid3.mp4', 'relatedness' : '0.4' },
+							{'id': 'vid4', 'type':'video','name': 'video 6', 'url': '/vid4.mp4', 'relatedness' : '0.3' }
+							]
+						},
+						{'id':'e1', 'timestamp': '35', 'related_objects' : [
+							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video','name': 'video 5', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video','name': 'video 6', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video','name': 'video 4', 'url': '/vid4.mp4', 'relatedness' : '0.2' }
+							]
+						},
+						]
+				};
