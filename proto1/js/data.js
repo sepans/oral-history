7c8370e7c36ac31523917a06cc5d8b26e5ddde92
sepans <sepans@sepans.com>
2013-01-18 17:43:49 -0500

    basic navigation

--- proto1/js/data.js
+++ proto1/js/data.js
@@ -1,37 +1,164 @@
-var videoInfo = {'id':'vid1', 'url': '/vid1.mp4', 'events': [ 
+var videoInfo =  {
+				'vid1':{'id':'vid1', 'title': 'Jean-Daniel Nicoud', 'url': '/vid1.mp4', 'vimeo_url': 'http://player.vimeo.com/video/45371204', 'events': [ 
 						{'id':'e1', 'timestamp': '0', 'related_objects' : [
-							{'id': 'vid2', 'type':'video', 'name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.8' },
-							{'id': 'vid3', 'type':'video','name': 'video 3','url': '/vid3.mp4', 'relatedness' : '0.6' },
-							{'id': 'vid4', 'type':'video','name': 'video 4','url': '/vid4.mp4', 'relatedness' : '0.4' }
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
 							]
 						},
-						{'id':'e1', 'timestamp': '8', 'related_objects' : [
-							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.7' },
-							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.7' },
-							{'id': 'vid5', 'type':'video','name': 'video 5', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
-							{'id': 'vid6', 'type':'video','name': 'video 6', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
+						{'id':'e1', 'timestamp': '10', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.9' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
 							]
 						},
-						{'id':'e1', 'timestamp': '15', 'related_objects' : [
-							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.8' },
-							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.6' },
-							{'id': 'vid4', 'type':'video','name': 'video 4', 'url': '/vid4.mp4', 'relatedness' : '0.4' }
+						{'id':'e1', 'timestamp': '45', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
 							]
 						},
-						{'id':'e1', 'timestamp': '22', 'related_objects' : [
-							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.7' },
-							{'id': 'vid3', 'type':'video','name': 'video 5', 'url': '/vid3.mp4', 'relatedness' : '0.4' },
-							{'id': 'vid4', 'type':'video','name': 'video 6', 'url': '/vid4.mp4', 'relatedness' : '0.3' }
+						{'id':'e1', 'timestamp': '80', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.4' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.3' }
 							]
 						},
-						{'id':'e1', 'timestamp': '35', 'related_objects' : [
-							{'id': 'vid2', 'type':'video','name': 'video 2', 'url': '/vid2.mp4', 'relatedness' : '0.8' },
-							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.7' },
-							{'id': 'vid5', 'type':'video','name': 'video 5', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
-							{'id': 'vid6', 'type':'video','name': 'video 6', 'url': '/vid4.mp4', 'relatedness' : '0.6' },
-							{'id': 'vid3', 'type':'video','name': 'video 3', 'url': '/vid3.mp4', 'relatedness' : '0.3' },
-							{'id': 'vid4', 'type':'video','name': 'video 4', 'url': '/vid4.mp4', 'relatedness' : '0.2' }
+						{'id':'e1', 'timestamp': '120', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.2' }
 							]
 						},
 						]
-				};
+				},
+				'vid2':{'id':'vid2', 'title': 'Minoru Asada', 'url': '/vid2.mp4',  'vimeo_url': 'http://player.vimeo.com/video/49698219', 'events': [ 
+						{'id':'e1', 'timestamp': '0', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '20', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							]
+						},
+						{'id':'e1', 'timestamp': '120', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.2' }
+							]
+						},
+						]
+				},
+				'vid3':{'id':'vid3', 'title': 'Gerd Herzinger', 'url': '/vid3.mp4',  'vimeo_url': 'http://player.vimeo.com/video/46409544', 'events': [ 
+						{'id':'e1', 'timestamp': '0', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '20', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							]
+						},
+						{'id':'e1', 'timestamp': '120', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.2' }
+							]
+						},
+						]
+				},
+				'vid4':{'id':'vid4', 'title': 'Ralph Hollis', 'url': '/vid3.mp4',  'vimeo_url': 'http://player.vimeo.com/video/46571035', 'events': [ 
+						{'id':'e1', 'timestamp': '0', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '20', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							]
+						},
+						{'id':'e1', 'timestamp': '120', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.2' }
+							]
+						},
+						]
+				},
+				'vid5':{'id':'vid5', 'title': 'Kazuhiro Kosuge', 'url': '/vid3.mp4',  'vimeo_url': 'http://player.vimeo.com/video/56381311', 'events': [ 
+						{'id':'e1', 'timestamp': '0', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '20', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							]
+						},
+						{'id':'e1', 'timestamp': '120', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.2' }
+							]
+						},
+						]
+				},
+				'vid6':{'id':'vid6', 'title': 'Vijay Kumar', 'url': '/vid4.mp4',  'vimeo_url': 'http://player.vimeo.com/video/49088612', 'events': [ 
+						{'id':'e1', 'timestamp': '0', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.4' }
+							]
+						},
+						{'id':'e1', 'timestamp': '20', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							]
+						},
+						{'id':'e1', 'timestamp': '120', 'related_objects' : [
+							{'id': 'vid2', 'type':'video', 'relatedness' : '0.8' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.7' },
+							{'id': 'vid5', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid6', 'type':'video', 'relatedness' : '0.6' },
+							{'id': 'vid3', 'type':'video', 'relatedness' : '0.3' },
+							{'id': 'vid4', 'type':'video', 'relatedness' : '0.2' }
+							]
+						},
+						]
+				}
+				};							
